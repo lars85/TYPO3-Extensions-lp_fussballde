5e90df792652edff89e45ee13f7cf0cede801f85
Lars Peipmann <lp@lightwerk.com>
2013-08-04 13:25:08 +0200

    [TASK] Uploaded new version 3.03 to TER

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "lp_fussballde".
  *
- * Auto generated 21-07-2013 22:01
+ * Auto generated 04-08-2013 13:21
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
@@ -27,14 +27,18 @@ $EM_CONF[$_EXTKEY] = array (
 	'modify_tables' => '',
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
-	'version' => '3.0.2',
-	'constraints' => array(
-		'depends' => array(
+	'version' => '3.0.3',
+	'constraints' => 
+	array (
+		'depends' => 
+		array (
 			'typo3' => '6.0.0-6.1.99',
 		),
-		'conflicts' => array(
+		'conflicts' => 
+		array (
 		),
-		'suggests' => array(
+		'suggests' => 
+		array (
 		),
 	),
 );
